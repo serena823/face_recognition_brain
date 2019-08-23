db34224eebac38a45e15722acc7d7191f3a552bd
siqi <520serki@gmail.com>
2019-08-23 14:47:37 -0400

    frontend

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import Navigation from './components/Navigation/Navigation.js'
+import Logo from './components/Logo/Logo.js'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
+import Rank from './components/Rank/Rank.js'
+import './App.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number:{
+      value:30,
+      density: {
+        enable:true,
+        value_area:800
+      }
+    }             
+   }
+
+}
+
+
+
+class App extends Component {
+  render(){
+    return (
+    <div className="App">
+          <Particles className='particles'
+             params={ particlesOptions }
+          />
+       <Navigation />
+       <Logo />
+       <Rank />
+       <ImageLinkForm />
+       {  /*
+             <ImageLinkForm />
+          <faceRecognition /> */ }
+    </div>
+  );
+  }
+  
+}
+
+export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import brain from './brain.png';
+
+const Logo = () => {
+	return(
+		<div className='ma4 mt0'>
+			<Tilt className="Tilt br2 shadow-2" options={{ max: 55 }} style={{ height: 75, width: 75 }} >
+		        <div className="Tilt-inner pa3"> 
+			        <img style={{padingTop:'5px'}} alt='logo' src={brain}/> 
+		        </div>
+	        </Tilt>
+		</div>
+		 
+
+		);
+}
+
+export default Logo;
